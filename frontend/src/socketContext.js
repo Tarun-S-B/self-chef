26492fe981d18c9df99bd7e58aee2858801e1d33
tarun <bagewaditarun@gmail.com>
2025-03-18 14:16:39 +0530

    socket context wss

--- frontend/src/socketContext.js
+++ frontend/src/socketContext.js
@@ -7,7 +7,7 @@ export const SocketProvider = ({ children }) => {
     const [socket, setSocket] = useState(null);
 
     useEffect(() => {
-        const newSocket = io("https://self-chef-igom.onrender.com", { transports: ["websocket"], secure: true });
+        const newSocket = io("wss://self-chef-igom.onrender.com", { transports: ["websocket"], secure: true });
 
         newSocket.on("connect", () => console.log("Connected:", newSocket.id));
         newSocket.on("disconnect", () => console.log("Disconnected"));
